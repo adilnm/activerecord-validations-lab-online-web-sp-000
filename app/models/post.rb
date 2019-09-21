35385a93b55cba3470c4ff24bc4e1bc4df4638fe
ADIL NAIMI <adilnm23@hotmail.com>
2019-09-21 03:12:55 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
   validate :is_clickbait?
-  
+
   CLICKBAIT_PATTERNS = [/Won't Believe/i,/Secret/i,/Top [0-9]*/i,/Guess/i]
 
   def is_clickbait?
